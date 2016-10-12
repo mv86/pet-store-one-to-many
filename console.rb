92f4c165d976a5319b1ec62ef8c47f22b7797d0f
Max <mveasey87@gmail.com>
2016-10-12 15:26:48 +0100

    added new pets and pet store

--- console.rb
+++ console.rb
@@ -6,6 +6,9 @@ require('pry-byebug')
 pets_emporium = PetStore.new({'name' => 'Pets Emporium', 'address' => '123 Fake St'})
 pets_emporium.save()
 
+monkey_market = PetStore.new({'name' => 'Monkeys Market', 'address' => '789 Banana Grove'})
+monkey_market.save()
+
 pet1 = Pets.new({'name' => 'Dave', 'type' => 'Monkey', 'pet_store_id' => pets_emporium.id})
 pet1.save()
 pet2 = Pets.new({'name' => 'Maurice', 'type' => 'Monkey', 'pet_store_id' => pets_emporium.id})
@@ -14,6 +17,13 @@ pet3 = Pets.new({'name' => 'Kev', 'type' => 'Dog', 'pet_store_id' => pets_empori
 pet3.save()
 pet4 = Pets.new({'name' => 'Timmy', 'type' => 'Snake', 'pet_store_id' => pets_emporium.id})
 pet4.save()
+
+pet5 = Pets.new({'name' => 'Steve', 'type' => 'Silverback', 'pet_store_id' => monkey_market.id})
+pet5.save()
+pet6 = Pets.new({'name' => 'Gordon', 'type' => 'Gorilla', 'pet_store_id' => monkey_market.id})
+pet6.save()
+
+
 
 binding.pry
 nil
